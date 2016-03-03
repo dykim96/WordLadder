f0801b8f96f95e156b13ac0e377f1aaf01782a6a
RohanTanna <rohantanna6@gmail.com>
2016-03-03 01:40:52 -0600

    Update WordLadderSolver.java

--- src/assignment4/WordLadderSolver.java
+++ src/assignment4/WordLadderSolver.java
@@ -2,23 +2,49 @@
     ADD YOUR HEADER HERE
  */
 
+
 package assignment4;
 
+import java.util.ArrayList;
+//import java.util.HashMap;
 import java.util.List;
 
 // do not change class name or interface it implements
 public class WordLadderSolver implements Assignment4Interface
 {
-    // delcare class members here.
+    // declare class members here.
+	
+//	HashMap<Integer, String> dictionary;
+	ArrayList<String> dictionary;
+	
+	public WordLadderSolver(String[] inputDict){
+		
+		 //insert code to parse the input dictionary, and set dictionary equal to this 
+		dictionary = new ArrayList<String>();
+		dictionary.add("heads");
+		dictionary.add("tails");
+		dictionary.add("heals");
+		dictionary.add("teals");
+		dictionary.add("tells");
+		dictionary.add("tolls");
+		dictionary.add("toils");
 
-    // add a constructor for this object. HINT: it would be a good idea to set up the dictionary there
+	}
+	
+
 
     // do not change signature of the method implemented from the interface
     @Override
     public List<String> computeLadder(String startWord, String endWord) throws NoSuchLadderException 
     {
         // implement this method
-        throw new UnsupportedOperationException("Not implemented yet!");
+    	ArrayList<String> solutionList = new ArrayList<String>();
+    	
+    	solutionList.add(startWord);
+    	solutionList = MakeLadder(startWord, endWord, 5, solutionList); //initially pass in 5 because you haven't changed any. in the top most call, you want to change all 5 letters
+    	
+    	return solutionList;
+      // new UnsupportedOperationException("Not implemented yet!");
     }
 
     @Override
@@ -26,6 +52,59 @@ public class WordLadderSolver implements Assignment4Interface
     {
         throw new UnsupportedOperationException("Not implemented yet!");
     }
+    
+    
 
     // add additional methods here
+    public  ArrayList<String> MakeLadder(String startWord, String endWord, int index, ArrayList<String> solutionList){
+    	
+    	
+    	
+    	
+    	ArrayList<String> returnList = new ArrayList<String>();// intentionally initialized to empty. if any paths reach a dead end, they should return an empty arraylist
+    	ArrayList<String> tempList = solutionList;
+    	for (int i = 0; i < 4; i++){
+    		for (char l = 'a'; l <= 'z'; l++){
+    			
+    			if (i != index){ //test all indices except the current one
+    				String tempString;
+    				if (i==4){
+    					tempString = startWord.substring(0, 4) + l;
+    					
+    				}
+    				else{
+    				 tempString = startWord.substring(0, i) + l + startWord.substring(i+1, 5); //replaces the current word with one letter
+    				}
+    				if (tempString.matches(endWord)){
+    					tempList.add(tempString);
+        				return tempList;
+
+    				}
+    				//haven't found the word yet:( keep searching by making recursive calls to all valid permutations 
+    				if ((!tempString.matches(startWord))&&(!tempList.contains(tempString)) &&(dictionary.contains(tempString)) ){ 
+    					//need to check 3 things before making recursive call
+    						//1. the new word you formed is an actual word
+    						//2. the word you are testing is not already in the list 
+    						//3. the word you are testing is not the word you passed lol
+    					tempList.add(tempString);
+    					returnList = MakeLadder(tempString, endWord, i, tempList);
+    					
+    					
+    				}
+    				
+    			}
+    		}
+    	}
+    	
+    	
+    	// if one of the permutations reaches the end word, the base case will trigger, and return the full list.
+    	// all of the permutations that dont reach the end return an empty list
+    	return returnList;
+    	
+    	
+    }
+    
+  
+    
+    
 }
